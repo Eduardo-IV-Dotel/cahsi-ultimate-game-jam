99a7e78a23cca7bc756d6f131efedcf79a812732
Pedro Juan Bonilla Morales <juan.bonilla13@upr.edu>
2026-03-27 23:31:39 -0400

    animations added

--- scripts/player_control.gd
+++ scripts/player_control.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var battery := 100.0
+var max_battery := 100.0
+var drain_rate := 10.0   
+var recharge_rate := 5.0 
 
 
 func _physics_process(delta: float) -> void:
@@ -21,5 +25,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	batery -= drain_rate * delta
+	battery = clamp(battery, 0, max_battery)
 	move_and_slide()
