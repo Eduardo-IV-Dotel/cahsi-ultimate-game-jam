b6f073ddb58c7fcfb234f280c2a5b71c23da6eba
Pedro Juan Bonilla Morales <juan.bonilla13@upr.edu>
2026-03-30 16:27:08 -0400

    update menu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -15,7 +15,7 @@ func _on_exit_pressed() -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/areas/area_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/areas/main_level.tscn")
 
 
 
