384ebf7bc223099700d824598dc00df046435adb
Pedro Juan Bonilla Morales <juan.bonilla13@upr.edu>
2026-03-30 11:24:05 -0400

    menu is connected to player

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -5,13 +5,9 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-
 
 func _on_exit_pressed() -> void:
 	print("exit")
