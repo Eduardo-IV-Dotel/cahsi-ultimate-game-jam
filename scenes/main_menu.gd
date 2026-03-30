46dcc1a98b3367ce71c17f608c8c42637f7d9da1
Pedro Juan Bonilla Morales <juan.bonilla13@upr.edu>
2026-03-30 16:24:38 -0400

    controls manu added

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -17,5 +17,7 @@ func _on_exit_pressed() -> void:
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/areas/area_1.tscn")
 
-func _on_exit_pressed() -> void:
-	get_tree().quit()
+
+
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/areas/controls_menu.tscn")
